612f3995a18b746a1e0de91c1ab9fb9117953b5b
Nathenael Dereb <nathnaelmelkamu@gmail.com>
2020-11-18 23:39:23 -0500

    baum-welch alg - testing on class example

--- baum_welch.py
+++ baum_welch.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from numpy import ndarray, zeros
 '''
 The Baum–Welch Algorithm
 '''
@@ -15,20 +15,41 @@ class BaumWeltch():
     num_state: number of states
     """
 
-    def __init__(self, S, O, pi):
-        self.pi = pi
+    def __init__(self, S, O, start_value=1, obs_seq=None):
         self.S = S
         self.O = O
-        self.num_obs = 0
-        self.num_state = 0
+        self.start_value = start_value
+        self.obs_seq = obs_seq
+        self.num_state = self.S.shape[0]
+        self.num_obs = 9 #self.O.shape[0]
 
         print('here')
 
     def forward(self):
         print('Forward Alg')
 
+        # matrix -> num_state x num_obs
+        a = zeros((self.num_obs+2, self.num_state))
+
+        # Initializes a[*][START] to self.start_value
+        a[:, 0] = self.start_value
+
+        print(a)
+        for i in range(1, self.num_obs):
+            for k in range(self.num_state):
+                # probability_objs = self.O[k, self.obs_seq[i]]
+                a[i,k] += np.dot(a[i-1, :], (self.S[:,k])) # * p_objs
+
+        print(a)
+        return a
+
     def backward(self):
         print('Backward Alg')
 
 
-BaumWeltch()
+transition = [[.7,.2,.1], [.15,.8,.05], [.6,.35,.05]]
+emission = [[.7,.2,.05,.05], [.2,.6,.1,.1]]
+observations = []
+
+BaumWeltch(np.array(transition), emission).forward()
+# BaumWeltch(np.ndarray(transition), np.ndarray(emission)).forward()
