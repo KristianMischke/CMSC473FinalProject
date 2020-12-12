91cf0075b2ceeaa709d15c25e107210431403d0d
Nathenael Dereb <nathnaelmelkamu@gmail.com>
2020-12-12 18:29:47 -0500

    corpus_pll fix

--- hmm.py
+++ hmm.py
@@ -226,11 +226,11 @@ class HMM:
     # Computes the perplexity of a corpus
     def compute_corpus_perplexity(self, sentences):
         N = len(sentences)
-        joint_prob = 1
+        joint_log_prob = 0
         for sent in sentences:
             log_marginal_likelihood = self.forward(sent, log_space=True)[-1, self.end_state]
-            joint_prob *= log_marginal_likelihood
-        return np.exp2((-1 / N) * joint_prob)
+            joint_log_prob += log_marginal_likelihood
+        return np.exp2((-1 / N) * joint_log_prob)
 
     def em_update(self, obs_seq):
         c_obs, c_trans, c_prlg = self.expectation_maximization(obs_seq, log_space=True)
